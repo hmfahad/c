1cd11e5eb84ff7ef79ed96453807e559306b39f1
H. M. Fahad <45889184+hmfahad@users.noreply.github.com>
2019-04-19 18:45:49 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int floor_Ceil(float n)
+{
+  float frection;
+
+  frection=n-(int)n;
+
+  if(frection>0.49)
+
+    return (int)n+1;
+  else
+
+    return (int)n;
+
+}
+
+
+int main()
+{
+    float n;
+
+    scanf("%f", &n);
+
+
+    printf("%d", floor_Ceil(n));
+    return 0;
+}
